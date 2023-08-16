b2a850d5451d4564bbca30ccda6a8688578fdebd
PatHightree <patrick.hogenboom@pm.me>
2023-08-16 19:41:27 +0000

    Each day a different version of the yellow beatle is set as my avatar

--- api/index.js
+++ api/index.js
@@ -1,33 +1,37 @@
 const { WebClient } = require('@slack/web-api');
 const axios = require('axios').default;
-const images = {
-  "morning": "https://images.unsplash.com/photo-1517188206596-1e1f7c954177?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-  "afternoon": "https://images.unsplash.com/photo-1566452348683-91f934cd9051?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2090&q=80",
-  "night": "https://images.unsplash.com/photo-1519446251021-1c4ee77fec1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=913&q=80"
-} 
+const images = []
+images.push(
+  "https://imgur.com/RPfmS7J.jpg",
+  "https://imgur.com/xhZ6Vzd.jpg",
+  "https://imgur.com/xjx9LQu.jpg",
+  "https://imgur.com/CIoJmk1.jpg",
+  "https://imgur.com/RBFytxD.jpg",
+  "https://imgur.com/Tb3GkMs.jpg",
+  "https://imgur.com/RwSvwy6.jpg",
+  "https://imgur.com/hkQEiU0.jpg",
+  "https://imgur.com/7kGNkSz.jpg")
+
+mod = function (m, n) {
+  return ((m % n) + n) % n
+}
+
 async function setPFP() {
-  var hour = new Date().getHours() + 8
+  var startDay = 17
+  var day = new Date().getDate() - startDay
+  var index = mod(day, images.length)
+  // console.log(images[index])
+
   let image
-  if (5 < hour && hour < 12) {
-    image = await axios.get(images.morning, {
-      responseType: "arraybuffer",
-    });
-  }
-  else if (12 < hour && hour < 20) {
-    image = await axios.get(images.afternoon, {
-      responseType: "arraybuffer",
-    });
-  }
-  else {
-    image = await axios.get(images.night, {
-      responseType: "arraybuffer",
-    });
-  }
+  image = await axios.get(images[index], {
+    responseType: "arraybuffer",
+  });
+  
   const client = new WebClient();
-  // const slackRequest = await client.users.setPhoto({
-  //   image: image.data,
-  //   token: process.env.SLACK_TOKEN
-  // });
+  const slackRequest = await client.users.setPhoto({
+    image: image.data,
+    token: process.env['SLACK_TOKEN']
+  });
 }
 
 export default async (req, res) => {
